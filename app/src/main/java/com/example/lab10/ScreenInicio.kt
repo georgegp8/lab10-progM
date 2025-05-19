099c98fca337c4eff745272521c267a079ebf4d0
george <george.guerra@tecsup.edu.pe>
2025-05-19 12:40:42 -0500

    feat(inicio): agregar pantalla de inicio con imagen centrada desde recursos drawable

--- app/src/main/java/com/example/lab10/ScreenInicio.kt
+++ app/src/main/java/com/example/lab10/ScreenInicio.kt
@@ -1,2 +1,28 @@
 package com.example.lab10
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ScreenInicio() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Cargar y mostrar la imagen desde los recursos drawable usando painterResource
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground), // Reemplaza 'tu_imagen' por el nombre del recurso drawable
+            contentDescription = "Imagen de ejemplo",
+            modifier = Modifier
+                .fillMaxSize()
+
+        )
+    }
+}
