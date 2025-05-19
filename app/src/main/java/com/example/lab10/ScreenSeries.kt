689a47e416fb891a35f76be64c44a73522ce67d2
george <george.guerra@tecsup.edu.pe>
2025-05-19 12:29:45 -0500

    feat(ui): agregar función composable para editar series obtenidas del API

--- app/src/main/java/com/example/lab10/ScreenSeries.kt
+++ app/src/main/java/com/example/lab10/ScreenSeries.kt
@@ -22,7 +22,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
 
 
 @Composable
@@ -64,31 +71,131 @@ fun ContenidoSeriesListado(navController: NavHostController, servicio: SerieApiS
 
         items(listaSeries) { item ->
             Row(
-                modifier = Modifier.padding(start=8.dp).fillParentMaxWidth(),
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .fillParentMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(text = "${item.id}", fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier=Modifier.weight(0.1f))
-                Text(text = item.name, fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier=Modifier.weight(0.6f))
+                Text(
+                    text = "${item.id}",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(0.1f)
+                )
+                Text(
+                    text = item.name,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(0.6f)
+                )
                 IconButton(
                     onClick = {
                         navController.navigate("serieVer/${item.id}")
-                        Log.e("SERIE-VER","ID = ${item.id}")
+                        Log.e("SERIE-VER", "ID = ${item.id}")
                     },
                     Modifier.weight(0.1f)
                 ) {
-                    Icon(imageVector = Icons.Outlined.Edit, contentDescription = "Ver", modifier=Modifier.align(Alignment.CenterVertically))
+                    Icon(
+                        imageVector = Icons.Outlined.Edit,
+                        contentDescription = "Ver",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
                 }
                 IconButton(
                     onClick = {
                         navController.navigate("serieDel/${item.id}")
-                        Log.e("SERIE-DEL","ID = ${item.id}")
+                        Log.e("SERIE-DEL", "ID = ${item.id}")
                     },
                     Modifier.weight(0.1f)
                 ) {
-                    Icon(imageVector = Icons.Outlined.Delete, contentDescription = "Ver", modifier=Modifier.align(Alignment.CenterVertically))
+                    Icon(
+                        imageVector = Icons.Outlined.Delete,
+                        contentDescription = "Ver",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun ContenidoSerieEditar(navController: NavHostController, servicio: SerieApiService, pid: Int = 0 ) {
+    var id by remember { mutableStateOf<Int>(pid) }
+    var name by remember { mutableStateOf<String?>("") }
+    var release_date by remember { mutableStateOf<String?>("") }
+    var rating by remember { mutableStateOf<String?>("") }
+    var category by remember { mutableStateOf<String?>("") }
+    var grabar by remember { mutableStateOf(false) }
+
+    if (id != 0) {
+        LaunchedEffect(Unit) {
+            val objSerie = servicio.selectSerie(id.toString())
+            delay(100)
+            name = objSerie.body()?.name
+            release_date = objSerie.body()?.release_date
+            rating = objSerie.body()?.rating.toString()
+            category = objSerie.body()?.category
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        // Spacer(Modifier.height(50.dp))
+        TextField(
+            value = id.toString(),
+            onValueChange = { },
+            label = { Text("ID (solo lectura)") },
+            readOnly = true,
+            singleLine = true
+        )
+        TextField(
+            value = name!!,
+            onValueChange = { name = it },
+            label = { Text("Name: ") },
+            singleLine = true
+        )
+        TextField(
+            value = release_date!!,
+            onValueChange = { release_date = it },
+            label = { Text("Release Date:") },
+            singleLine = true
+        )
+        TextField(
+            value = rating!!,
+            onValueChange = { rating = it },
+            label = { Text("Rating:") },
+            singleLine = true
+        )
+        TextField(
+            value = category!!,
+            onValueChange = { category = it },
+            label = { Text("Category:") },
+            singleLine = true
+        )
+        Button(
+            onClick = {
+                grabar = true
+            }
+        ) {
+            Text("Grabar", fontSize=16.sp)
+        }
+    }
+
+    if (grabar) {
+        val objSerie = SerieModel(id,name!!, release_date!!, rating!!.toInt(), category!!)
+        LaunchedEffect(Unit) {
+            if (id == 0)
+                servicio.insertSerie(objSerie)
+            else
+                servicio.updateSerie(id.toString(), objSerie)
+        }
+        grabar = false
+        navController.navigate("series")
     }
 }
 
