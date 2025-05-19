07624e6ad2f2b3b9835c37eb3990cea2c728123b
george <george.guerra@tecsup.edu.pe>
2025-05-19 12:33:24 -0500

    feat(ui): agregar función composable para eliminar series obtenidas del API

--- app/src/main/java/com/example/lab10/ScreenSeries.kt
+++ app/src/main/java/com/example/lab10/ScreenSeries.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
@@ -199,3 +200,39 @@ fun ContenidoSerieEditar(navController: NavHostController, servicio: SerieApiSer
     }
 }
 
+@Composable
+fun ContenidoSerieEliminar(navController: NavHostController, servicio: SerieApiService, id: Int) {
+    var showDialog by remember { mutableStateOf(true) }
+    var borrar by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = "Confirmar Eliminación") },
+            text = { Text("¿Está seguro de eliminar la Serie?") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        showDialog = false
+                        borrar = true
+                    }) {
+                    Text("Aceptar")
+                }
+            },
+            dismissButton = {
+                Button(onClick = { showDialog = false }) {
+                    Text("Cancelar")
+                    navController.navigate("series")
+                }
+            }
+        )
+    }
+    if (borrar) {
+        LaunchedEffect(Unit) {
+            // val objSerie = servicio.selectSerie(id.toString())
+            servicio.deleteSerie(id.toString())
+            borrar = false
+            navController.navigate("series")
+        }
+    }
+}
